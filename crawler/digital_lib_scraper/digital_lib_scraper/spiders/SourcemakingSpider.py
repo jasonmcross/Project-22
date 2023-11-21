53cb066aeb2397be5ff18f2dd9bafc3754e0db29
Yatniel Ramos <yatnieljose0208@gmail.com>
2023-11-22 00:49:59 +0100

    Created SourceMaking spider

--- crawler/digital_lib_scraper/digital_lib_scraper/spiders/SourcemakingSpider.py
+++ crawler/digital_lib_scraper/digital_lib_scraper/spiders/SourcemakingSpider.py
@@ -1,14 +1,11 @@
 import scrapy
-from scrapy.spiders import CrawlSpider, Rule
 import json
 
-class StackoverflowSpider(scrapy.Spider):
-    name = "stackoverflow"
-    allowed_domains = ["stackoverflow.blog"]
-    start_urls = [# content after h3 element
-        #"https://stackoverflow.com/questions/1673841/examples-of-gof-design-patterns-in-javas-core-libraries",
-         "https://stackoverflow.blog/2021/10/13/why-solve-a-problem-twice-design-patterns-let-you-apply-existing-solutions-to-your-code/"
-    ]
+
+class SpringframeworkSpider(scrapy.Spider):
+    name = "sourcemaking"
+    allowed_domains = ["sourcemaking.com"]
+    start_urls = ["https://https://sourcemaking.com/design_patterns"]
 
     def parse(self, response):
         
