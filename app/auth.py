e5f863e9c88b5c84e7b5de044bf3df16a973906f
Jason Cross <jason.m.cross@gmail.com>
2023-12-08 11:15:19 -0500

    Initial commit

---
+++ app/auth.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    data = request.form
+    return render_template("login.html")
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+         if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            #new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+            #    password1, method='sha256'))
+            #db.session.add(new_user)
+            #db.session.commit()
+            #login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            #return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html")
