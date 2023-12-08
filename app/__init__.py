7dd4bffcee8caeb233f31a30b390e1fcb3361e1f
Jason Cross <jason.m.cross@gmail.com>
2023-12-08 17:46:43 -0500

    Ready to tweak

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'project 22'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)   
 
     from .views import views
     from .auth import auth
@@ -10,4 +18,22 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader # This decorator is used to reload the user object from the user ID stored in the session
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+def create_database(app):
+    if not path.exists('app/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
