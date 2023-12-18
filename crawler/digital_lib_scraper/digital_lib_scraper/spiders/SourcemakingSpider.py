4c4554a93c5a42067556eead500a3fad22d569ac
Yatniel Ramos <yatnieljose0208@gmail.com>
2023-12-18 22:34:19 +0100

    Modified spider and added training data
    Spider now parses through client website
    Only parses problem domain
    
    CSV file added with three fields
    Category, Pattern, Data

--- crawler/digital_lib_scraper/digital_lib_scraper/spiders/SourcemakingSpider.py
+++ crawler/digital_lib_scraper/digital_lib_scraper/spiders/SourcemakingSpider.py
@@ -1,5 +1,5 @@
 import scrapy
-import json
+import csv
 import os
 
 class SourcemakingSpider(scrapy.Spider):
@@ -24,8 +24,35 @@ class SourcemakingSpider(scrapy.Spider):
 
     def parse_patterns(self, response):
 
+        sections = ("Intent", "Problem", "Motivation", "Discussion")
         resp = response.xpath("//article")
-        h1 = resp.xpath("//h1")#/strong/text()").get()#.xpath("string()").get()
+        elements = resp.xpath("//h3 | //p | //ul")
 
-        category = response.meta.get("categories")
-        print(h1.xpath("string()").get().replace(" Design Pattern", ""))
+        category = response.meta.get("categories").xpath("string()").get().replace(" patterns", "")
+        pattern = resp.xpath("//h1").xpath("string()").get().replace(" Design Pattern", "") 
+        data = ""
+        h3 = False
+
+        for element in elements:
+            if element.xpath("name()").extract_first() == "h3":
+                h3 = any(element.xpath("string()").get() == section for section in sections)
+            elif h3:
+                if element.xpath("name()").extract_first() == "ul":
+                    for li in element.xpath("./li"):
+                        data += li.xpath("string()").get()
+                elif element.xpath("name()").extract_first() == "p":
+                    data += element.xpath("string()").get()
+        
+        out_data = {
+            "Category": category,
+            "Pattern": pattern,
+            "Data": data
+        }
+
+        self.write_to_csv(out_data)
+
+    def write_to_csv(self, data):
+        with open(os.path.abspath(os.path.join(os.getcwd(), "../../../data/sourcemaking.csv")), "a", newline="", encoding="utf-8") as csvfile:
+            fieldnames = ["Category", "Pattern", "Data"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow(data)
