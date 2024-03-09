c0f7d04a008c36090211d617336705a971c22e12
TylerC53 <123029553+TylerC53@users.noreply.github.com>
2024-03-08 19:32:25 -0500

    Update auth.py

--- new_format/website/auth.py
+++ new_format/website/auth.py
@@ -20,7 +20,7 @@ def login():
             if user_data and user_data[0] == password:
                 session['logged_in'] = True
                 flash('Login successful!', category='success')
-                return redirect(url_for('auth.admin_home'))
+                return render_template("admin-home.html")
 
             else:
                 flash('Invalid email or password. Please try again.', category='error')
@@ -29,29 +29,33 @@ def login():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        con_password = request.form.get('con_password')
-        name = request.form.get('name')
+    if 'logged_in' not in session:
+        flash('Cannot view page', category='error')
+        return render_template("admin-login.html")
+    else:
+        if request.method == 'POST':
+            email = request.form.get('email')
+            password = request.form.get('password')
+            con_password = request.form.get('con_password')
+            name = request.form.get('name')
         
-        if len(email) < 8 or len(email) > 100 or len(password) < 8 or len(password) > 100 or password != con_password:
-            flash('Invalid input. Please try again.', category='error')
-        else:
-            # Add the new admin account to the database
-            db.add_user(email, name, password)
-            flash('Admin account created successfully.', category='success')
-            session['logged_in'] = True
-            return redirect(url_for('auth.admin_home'))
+            if len(email) < 8 or len(email) > 100 or len(password) < 8 or len(password) > 100 or password != con_password:
+                flash('Invalid input. Please try again.', category='error')
+            else:
+                # Add the new admin account to the database
+                db.add_user(email, name, password)
+                flash('Admin account created successfully.', category='success')
+                session['logged_in'] = True
+                return render_template("admin-home.html")
     
-    return render_template("admin-sign-up.html")
+        return render_template("admin-sign-up.html")
 
 @auth.route('/logout')
 def logout():
-    session.pop('logged_in', None)
-    return render_template("developer-home.html")
-
-@auth.route('/admin-home')
-def admin_home():
-    # Perform some logic
-    return render_template("admin-home.html")
+    if 'logged_in' not in session:
+        flash('Cannot view page', category='error')
+        return render_template("admin-login.html")
+    else:
+        session.pop('logged_in', None)
+        flash('Logged out sucessfully!', category='success')
+        return render_template("developer-home.html")
