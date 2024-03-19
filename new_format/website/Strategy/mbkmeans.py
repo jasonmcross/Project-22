4695cb19f92d06acb2d6ec5a6fd8d699442a6a6e
Yatniel Ramos <yatnieljose0208@gmail.com>
2024-03-19 22:16:10 +0100

    Modifies mbkmeans for use

--- new_format/website/Strategy/mbkmeans.py
+++ new_format/website/Strategy/mbkmeans.py
@@ -10,15 +10,20 @@ class MBKMeansClusterer(Clusterer):
     def __init__(self, num_clusters):
         super().__init__(num_clusters=num_clusters)
     
-    def cluster(self, df: pd.DataFrame, features):
+    def cluster(self, features):
         mbkm = MiniBatchKMeans(n_clusters=3, random_state = 0)
         mbkm.fit(features)
         pca = PCA(n_components=2, random_state = 0)
         reduced_features = pca.fit_transform(features.toarray())
         reduced_cluster_centers = pca.transform(mbkm.cluster_centers_)
+
+        for cluster_label in range(3):
+            cluster_points = reduced_features[mbkm.labels_ == cluster_label]
+            plt.scatter(cluster_points[:, 0], cluster_points[:, 1], label=f'Cluster {cluster_label}')
         
         plt.scatter(reduced_features[:,0], reduced_features[:,1], c=mbkm.labels_)
         plt.scatter(reduced_cluster_centers[:, 0], reduced_cluster_centers[:,1], marker='x', s=150, c='b')
+        plt.legend()
         plt.title("MiniBatchKMeans")
         plt.xlabel("PCA Feature 1")
         plt.ylabel("PCA Feature 2")
