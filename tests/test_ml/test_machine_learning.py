39e76ab9ed80cad213eb58996602af131a15d42d
Yatniel Jose Ramos Rivera <46455372+yatnieljose@users.noreply.github.com>
2024-04-13 20:49:34 +0200

    Updates test_machine_learning.py

--- tests/test_ml/test_machine_learning.py
+++ tests/test_ml/test_machine_learning.py
@@ -1,29 +1,89 @@
 import pytest
+import csv
 import os
 import sys
+import itertools
+from pathlib import Path
+import pandas as pd
+import pickle
 
-cwd = os.path.dirname(__file__)
-ml_path = os.path.abspath(os.path.join(cwd, "..", "..", "machinelearning"))
-sys.path.append(ml_path)
-os.chdir(ml_path)
+PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+DRIVER_PATH = os.path.join("website", "Strategy")
 
-import classifier
+sys.path.append(PROJECT_PATH)
+sys.path.append(DRIVER_PATH)
 
-key = [
-    ("Singleton Pattern", "A system needs to control access to a resource (like a database connection, a file, or a network resource) among multiple clients efficiently while ensuring that only one client accesses the resource at a time."),
-    ("Strategy Pattern", "Design a system where components can be easily added, removed, or replaced without affecting the overall system functionality."),
-    ("Factory Method Pattern", "Develop a graphical user interface (GUI) framework that allows users to create different types of UI elements (buttons, text fields, etc.) with custom behavior."),
-    ("Command Pattern", "Create a feature that allows users to undo or redo their actions in an application.")
-]
+from website.Strategy.BertPredictor import BertPredictor
+from website.Strategy.test import run_test
 
-@pytest.mark.parametrize("pattern, description", key)
-def test_return(pattern, description):
-    # Returns a collection of pattern prediction
-    resp = classifier.predict_design_pattern(description)
-    assert 3 == len(resp)
+# Reads CSV file for problems and answers
+def read_csv():
+    file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "answer_key.csv"))
+    data = []
+    with open(file_path, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            data.append((row['problem'], row['Answer']))
+    return data
 
-@pytest.mark.parametrize("pattern, description", key)
-def test_predict(pattern, description):
-    # Returns a collection of pattern prediction
-    resp = classifier.predict_design_pattern(description)
-    assert pattern == resp[0][0]
+# Tests Bert predictor
+def test_bert_accuracy():
+    # Read answer key
+    answer_key = read_csv() # Adjust file name accordingly
+
+    pred = BertPredictor(os.path.abspath(os.path.join(os.path.dirname(__file__), "masterGOF.csv")))
+
+    total_questions = len(answer_key)
+    
+    # Read system output
+    system_output = [] 
+    for i in range(total_questions):
+        system_output.append(pred.predict(answer_key[i][0]))
+    
+    # Calculate accuracy
+    correct_answers = 0
+    csv_entries = []
+    for i in range(total_questions):
+        csv_entries.append({answer_key[i][1]: system_output[i]})
+        if answer_key[i][1] == system_output[i][0]["Name"]:
+            correct_answers += 1
+        elif answer_key[i][1] == system_output[i][1]["Name"]:
+            correct_answers += 0.66
+        elif answer_key[i][1] == system_output[i][2]["Name"]:
+            correct_answers += 0.33
+        
+    accuracy = (correct_answers / total_questions) * 100
+    print(f"System accuracy is {accuracy}%, which is below 60%.")
+    
+    # Assert if accuracy meets the desired threshold
+    assert accuracy >= 60 #, f"System accuracy is {accuracy}%, which is below 60%."
+
+@pytest.mark.parametrize("vector, clusterer", itertools.product(range(1, 2), range(1, 3)))#range(1, 3), range(1, 9)))
+def test_custom_accuracy(vector, clusterer):
+    preprocess = [1, 1, 1, 1, 0, 0, 0, 0]
+
+    answer_key = read_csv() # Adjust file name accordingly
+    total_questions = len(answer_key)
+    system_output = [] #read_csv(system_output_file)
+    for i in range(total_questions):
+        system_output.append(run_test(preprocess, vector, clusterer, answer_key[i][0]))
+
+    print(len(system_output))
+
+    # Calculate accuracy
+    correct_answers = 0
+    csv_entries = []
+    for i in range(total_questions):
+        csv_entries.append({answer_key[i][1]: system_output[i]})
+        if answer_key[i][1] == system_output[i][0]:
+            correct_answers += 1
+        elif answer_key[i][1] == system_output[i][1]:
+            correct_answers += 0.66
+        elif answer_key[i][1] == system_output[i][2]:
+            correct_answers += 0.33
+        
+    accuracy = (correct_answers / total_questions) * 100
+    print(f"System accuracy is {accuracy}%, which is below 60%.")
+    
+    # Assert if accuracy meets the desired threshold
+    assert accuracy >= 60 #, f"System accuracy is {accuracy}%, which is below 60%."
