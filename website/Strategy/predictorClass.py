9deb8c8801750cec4f23f30351c60870a4fc5246
TylerC53 <123029553+TylerC53@users.noreply.github.com>
2024-04-15 11:15:52 -0400

    Update predictorClass.py

--- website/Strategy/predictorClass.py
+++ website/Strategy/predictorClass.py
@@ -1,11 +1,10 @@
-from CoR import extract_adjectives, extract_nouns, extract_verbs, lemmatize, lower_punc, remove_junk, remove_stop, stem, synonymize, tokenize
-import agglomerative, dbscan, fuzzyCmean, gaussianMixture, kmeans, mbkmeans, meanShift, spectral
-import defaultVectorizer, ngramVectorizer
+from website.Strategy.CoR import extract_adjectives, extract_nouns, extract_verbs, lemmatize, lower_punc, remove_junk, remove_stop, stem, synonymize, tokenize
+from website.Strategy import agglomerative, dbscan, fuzzyCmean, gaussianMixture, kmeans, mbkmeans, meanShift, spectral
+from website.Strategy import defaultVectorizer, ngramVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import numpy as np
 import pickle
-from CoR import remove_junk
 from pathlib import Path
 
 class Predictor:
