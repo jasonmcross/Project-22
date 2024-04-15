0c68e3a1c5aa580795c44d541a16176df4a929c1
TylerC53 <123029553+TylerC53@users.noreply.github.com>
2024-04-15 11:16:32 -0400

    Update main_dev.py

--- website/Strategy/main_dev.py
+++ website/Strategy/main_dev.py
@@ -1,52 +1,45 @@
-from asyncio.windows_events import NULL
-from predictorClass import Predictor
-from CoR import extract_adjectives, extract_nouns, extract_verbs, lemmatize, lower_punc, remove_junk, remove_stop, stem, synonymize, tokenize
-from Strategy import agglomerative, dbscan, fuzzyCmean, gaussianMixture, kmeans, mbkmeans, meanShift, spectral
-from Strategy import defaultVectorizer, ngramVectorizer
+from website.Strategy.CoR import lemmatize, lower_punc, remove_junk, remove_stop, stem, tokenize
+from website.Strategy import kmeans, mbkmeans
+from website.Strategy import defaultVectorizer
 from pathlib import Path
-from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
-import numpy as np
 import pickle
+from website.Strategy.predictorClass import Predictor
+from asyncio.windows_events import NULL
 
-def main(problem, collection, source, vector, clusterer, preprocess):
-    
-    # Access database to get what preprocessing was used and what vectorizer was used and model file path
-    preprocess = NULL
-    vector = NULL
-    modelPath = NULL
+def main(problem, collection):
+    print(collection)
     # Instantiate the selected preprocessors
-    preprocessors = [remove_junk(), stem(), tokenize(), lemmatize(), extract_nouns(), extract_verbs(), extract_adjectives(), synonymize()]
-    pp_user = [lower_punc(), remove_stop()]
-        
-    # Collect user input for selected preprocessors
-    for i, value in enumerate(preprocess):
-        if value == "1":
-            pp_user.append(preprocessors[i])
-
-    filepath = Path(__file__).parent / "models/" + modelPath
-    with open(filepath, 'rb') as model_file:
-        loaded_cls = pickle.load(model_file)   
+    preprocessors = [lower_punc.LowerPunc(), remove_stop.RemoveStop(), remove_junk.RemoveJunk(), stem.Stem(), tokenize.Tokenize(), lemmatize.Lemmatize()]
+    clusterer = "2"
     
-    # Instantiate the selected vectorizer
-    if vector == "1":
-        vectorizer = defaultVectorizer()
-    elif vector == "2":
-        vectorizer = ngramVectorizer()
+    # Get what clusterer to use
+    if clusterer == "1":
+        filepath = Path(__file__).parent / "models/kmeans_model.pkl"
+        with open(filepath, 'rb') as model_file:
+            loaded_cls = pickle.load(model_file)
+    elif clusterer == "2":
+        filepath = Path(__file__).parent / "models/mbkmeans_model.pkl"
+        with open(filepath, 'rb') as model_file:
+            loaded_cls = pickle.load(model_file)
+    
+    # Instantiate the vectorizer
+    vectorizer = defaultVectorizer.defaultVectorizer()
 
     # Load vectorizer
-    if vector == "1":
-        filepath = Path(__file__).parent / "vectorizers/vectorizer_default.pkl"
-        with open(filepath, 'rb') as vec_file:
-            loaded_vec = pickle.load(vec_file)
-    elif vector == "2":
-        filepath = Path(__file__).parent / "vectorizers/vectorizer_ngram.pkl"
-        with open(filepath, 'rb') as vec_file:
-            loaded_vec = pickle.load(vec_file)
+    filepath = Path(__file__).parent / "vectorizers/vectorizer_default.pkl"
+    with open(filepath, 'rb') as vec_file:
+        loaded_vec = pickle.load(vec_file)
+    
+    # Get what collection csv to use
+    path = "source_files/" + collection + ".csv"
+    filepath = Path(__file__).parent / path
+    df = pd.read_csv(filepath, encoding='ISO-8859-1',
+                   header=None, names=['Category', 'Pattern', 'Description']) 
 
-    p = Predictor(preprocess, vector, NULL)
+    p = Predictor(preprocessors, vectorizer, NULL)
+    df.iloc[:, 2] = df.iloc[:,2].astype(str).apply(p.preprocess_data)
     problem = p.preprocess_data(problem)
-    p.vectorize_data(problem)
-    results = p.predict(problem, pd.DataFrame, loaded_cls, loaded_vec)
+    results = p.predict(problem, df, loaded_cls, loaded_vec)
     
     return results
