05ec24d91e10d149f4ec2079153940de1fdad073
TylerC53 <123029553+TylerC53@users.noreply.github.com>
2024-04-25 12:54:31 -0400

    Update test.py

--- website/Strategy/test.py
+++ website/Strategy/test.py
@@ -1,5 +1,5 @@
 from website.Strategy.CoR import extract_adjectives, extract_nouns, extract_verbs, lemmatize, lower_punc, remove_junk, remove_stop, stem, synonymize, tokenize
-from website.Strategy import fuzzyCmean, kmeans, mbkmeans
+from website.Strategy import kmeans, mbkmeans
 from website.Strategy import defaultVectorizer
 from pathlib import Path
 import pandas as pd
@@ -20,7 +20,7 @@ def run_test(preprocess=["1", "1", "1", "1", "none", "none", "none", "none"], ve
     df = pd.read_csv(filepath, encoding='ISO-8859-1',
                    header=None, names=['Category', 'Pattern', 'Description'])    
     
-    
+    collection = "Test"
 
     # Collect user input for selected preprocessors
     for i, value in enumerate(preprocess):
@@ -36,37 +36,30 @@ def run_test(preprocess=["1", "1", "1", "1", "none", "none", "none", "none"], ve
         c = kmeans.KMeansClusterer(3)
     elif clusterer == 2:
         c = mbkmeans.MBKMeansClusterer(3)
-    elif clusterer == 3:
-        c = fuzzyCmean.FuzzyCMeansClusterer(3)
 
     predictor = Predictor(pp_user, v, c)
 
     df.iloc[:, 2] = df.iloc[:,2].astype(str).apply(predictor.preprocess_data)
 
-    features = predictor.vectorize_data(df)
-    predictor.cluster_data(features)
+    features = predictor.vectorize_data(df, collection)
+    predictor.cluster_data(features, collection)
 
     # Load vectorizer
     if vector == 1:
-        filepath = Path(__file__).parent / "vectorizers/vectorizer_default.pkl"
-        with open(filepath, 'rb') as vec_file:
-            loaded_vec = pickle.load(vec_file)
-    elif vector == 2:
-        filepath = Path(__file__).parent / "vectorizers/vectorizer_ngram.pkl"
+        path = f"vectorizers/{collection}.pkl"
+        filepath = Path(__file__).parent / path
         with open(filepath, 'rb') as vec_file:
             loaded_vec = pickle.load(vec_file)
     
     # Load model
     if clusterer == 1:
-        filepath = Path(__file__).parent / "models/kmeans_model.pkl"
+        path1 = f"models/{collection}KMeans.pkl"
+        filepath = Path(__file__).parent / path1
         with open(filepath, 'rb') as model_file:
             loaded_cls = pickle.load(model_file)
     elif clusterer == 2:
-        filepath = Path(__file__).parent / "models/mbkmeans_model.pkl"
-        with open(filepath, 'rb') as model_file:
-            loaded_cls = pickle.load(model_file)
-    elif clusterer == 3:
-        filepath = Path(__file__).parent / "models/fuzzycmeans_model.pkl"
+        path2 = f"models/{collection}MBKMeans.pkl"
+        filepath = Path(__file__).parent / path2
         with open(filepath, 'rb') as model_file:
             loaded_cls = pickle.load(model_file)
 
