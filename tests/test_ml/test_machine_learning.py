38b05e20f6584b228a479eed745c45f316eaeaed
Jason Cross <jason.m.cross@gmail.com>
2024-04-28 22:42:54 -0400

    Update test_machine_learning.py

--- tests/test_ml/test_machine_learning.py
+++ tests/test_ml/test_machine_learning.py
@@ -85,4 +85,4 @@ def test_custom_accuracy(vector, clusterer):
     print(f"System accuracy is {accuracy}%, which is below 60%.")
     
     # Assert if accuracy meets the desired threshold
-    assert accuracy >= 100
+    assert accuracy >= 60
