a93f3609eb391b6704b7b943ac739558285b1530
Jason Cross <jason.m.cross@gmail.com>
2024-04-28 22:39:10 -0400

    Final update, again.

--- tests/test_ml/test_machine_learning.py
+++ tests/test_ml/test_machine_learning.py
@@ -31,7 +31,8 @@ def test_bert_accuracy():
     # Read answer key
     answer_key = read_csv() # Adjust file name accordingly
 
-    pred = BertPredictor(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "website", "source_files", "masterGOF.csv")))
+    filepath = Path(__file__).parent.parent.parent / "website/Strategy/source_files/masterGOF.csv"
+    pred = BertPredictor(filepath)
 
     total_questions = len(answer_key)
     
